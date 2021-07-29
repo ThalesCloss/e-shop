87374bd4162b422d524e783df8d5fa3b233a2e3a
tcloss <thales.a.closs@gmail.com>
2021-07-28 23:57:58 -0400

    feat: updateCustomerUseCase implementation

---
+++ shop/application/customer/UpdateCustomerInteractor.ts
@@ -0,0 +1,30 @@
+import { DomainError } from '@shop/domain/base/DomainError';
+import { DomainReturn } from '@shop/domain/base/DomainReturn';
+import { Customer } from '@shop/domain/entities/Customer';
+import { GetCustomerUseCase } from '@shop/domain/useCases/customer/GetCustomerUseCase';
+import { UpdateCustomerUseCase } from '@shop/domain/useCases/customer/UpdateCustomerUseCase';
+import { CustomerRepository } from './contracts/CustomerRepository';
+
+export class UpdateCustomerInteractor
+  implements UpdateCustomerUseCase<Promise<DomainReturn<void>>>
+{
+  constructor(
+    private readonly customerRepository: CustomerRepository<
+      Promise<DomainReturn<Customer.CompleteCustomerData>>
+    >,
+    private readonly getCustomerUseCase: GetCustomerUseCase<
+      Promise<DomainReturn<Customer>>
+    >,
+  ) {}
+  async update(
+    updateCustomerData: Customer.CustomerData,
+    customerId: string,
+  ): Promise<DomainReturn<void, DomainError>> {
+    const customer = await this.getCustomerUseCase.get(customerId);
+    if (customer instanceof DomainError) return customer;
+    const canUpdate = customer.update(updateCustomerData);
+    if (canUpdate instanceof DomainError) return canUpdate;
+    await this.customerRepository.save(customer);
+    return;
+  }
+}
