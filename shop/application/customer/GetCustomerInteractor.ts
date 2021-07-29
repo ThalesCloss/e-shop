a92ae35b8b032c279ceb51a4ba07f0b7c21a8c84
tcloss <thales.a.closs@gmail.com>
2021-07-28 23:21:53 -0400

    feat: getCustomerUseCase implementation

---
+++ shop/application/customer/GetCustomerInteractor.ts
@@ -0,0 +1,25 @@
+import { DomainError } from '@shop/domain/base/DomainError';
+import { DomainReturn } from '@shop/domain/base/DomainReturn';
+import { EntityId } from '@shop/domain/base/valueObjects/EntityId';
+import { Customer } from '@shop/domain/entities/Customer';
+import { GetCustomerUseCase } from '@shop/domain/useCases/customer/GetCustomerUseCase';
+import { CustomerRepository } from './contracts/CustomerRepository';
+
+export class GetCustomerInteractor
+  implements GetCustomerUseCase<Promise<DomainReturn<Customer>>>
+{
+  constructor(
+    private readonly customerRepository: CustomerRepository<
+      Promise<DomainReturn<Customer.CompleteCustomerData>>
+    >,
+  ) {}
+
+  async get(customerId: string): Promise<DomainReturn<Customer>> {
+    const customerEntityId = EntityId.create(customerId);
+    if (customerEntityId instanceof DomainError) return customerEntityId;
+    const customer = await this.customerRepository.get(customerEntityId);
+    if (customer instanceof DomainError) return customer;
+    const costumerEntity = await Customer.create(customer, customer.id);
+    return costumerEntity;
+  }
+}
