20841f6b8fe775dd729db5da88f39b365c62032f
tcloss <thales.a.closs@gmail.com>
2021-07-28 21:03:48 -0400

    refactor: delete customer by EntityId

--- shop/application/customer/contracts/CustomerRepository.ts
+++ shop/application/customer/contracts/CustomerRepository.ts
@@ -3,6 +3,6 @@ import { Customer } from '@shop/domain/entities/Customer';
 
 export interface CustomerRepository<R> {
   save(customer: Customer): R;
-  delete(customer: Customer): R;
+  delete(customerEntityId: EntityId): R;
   get(customerEntityId: EntityId): R;
 }
